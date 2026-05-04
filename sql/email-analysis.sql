cbce2bc9fea90f1117dca0ee1e5e4dbe8f935d87
MariaKuziela <maria.kuziela@gmail.com>
2026-05-04 18:52:21 +0200

    Update email-analysis.sql

--- sql/email-analysis.sql
+++ sql/email-analysis.sql
@@ -40,15 +40,16 @@ GROUP BY Customer_Location ;
 --- Czy liczba linków w emailu (Total_Links) wpływa na success rate?
 
 
-SELECT
-    COUNT(Email_ID) AS total_emails
-    SUM(CASE WHEN Email_Status = 1 THEN 1 ELSE 0 END) AS positive_reactions,
-    CASE
-        WHEN TOTAL_LINKS IS NULL OR TOTAL_LINKS = 0 OR TOTAL_LINKS = ' ' THEN "0_LINKS"
-        WHEN TOTAL_LINKS >= 6 THEN ">6_LINKS"
-        ELSE "1-5_LINKS"
-    END AS "GROUPED_LINKS",
-    ROUND(SUM(CASE WHEN Email_Status = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(TOTAL_LINKS), 2) AS success_rate
+SELECT 
+	COUNT(Email_ID) AS total_emails,
+	SUM(CASE WHEN Email_Status = 1 THEN 1 ELSE 0 END) AS positive_reactions,
+	CASE 
+		WHEN TOTAL_LINKS = '' THEN "0_LINKS" 
+		WHEN TOTAL_LINKS >= 16 THEN ">16_LINKS"
+		WHEN TOTAL_LINKS BETWEEN 1 AND 5 THEN "1-5_LINKS"
+		ELSE "6-15_LINKS"
+	END AS "GROUPED_LINKS",
+	ROUND(SUM(CASE WHEN Email_Status = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(TOTAL_LINKS), 2) AS success_rate
 FROM email_campaigns
 WHERE Email_Status IN (0, 1)
 GROUP BY GROUPED_LINKS;
